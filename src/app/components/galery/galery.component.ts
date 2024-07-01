0d171f5d7325efe1bb44a7a5b687dca73291385b
JazminBriasco <briascojazmin@gmail.com>
2024-07-01 12:27:50 -0300

    redirect nav bug fixed

--- src/app/components/galery/galery.component.ts
+++ src/app/components/galery/galery.component.ts
@@ -7,11 +7,8 @@ import { BackgroundEnum } from 'src/app/utils/_const.enum';
   styleUrls: ['./galery.component.scss']
 })
 
-
 /**TODO
- * borde y sombra a las fotos
  * Spiner al enviar mail
- * Quitar y ver tema celular (wpp)
  */
 
 export class GaleryComponent implements OnInit {
@@ -37,11 +34,71 @@ export class GaleryComponent implements OnInit {
   bkg24 = BackgroundEnum.bkg15;
   bkg25 = BackgroundEnum.bkg16;
 
+  carousel = document.getElementById('.carousel');
 
+  images = [];
+  currentImage = 0;
+   slides: any;
   
-  constructor() { }
+  slides1 = [
+    {
+      title: '',
+      image: this.bkg18,
+      description: ''
+    },
+    {
+      title: '',
+      image: this.bkg25,
+      description: ''
+    },
+    {
+      title: '',
+      image: this.bkg24,
+      description: ''
+    }
+  ];
 
-  ngOnInit(): void {
-  }
+  slides2 = [
+    {
+      title: '',
+      image: this.bkg18,
+      description: ''
+    },
+    {
+      title: '',
+      image: this.bkg25,
+      description: ''
+    },
+    {
+      title: '',
+      image: this.bkg24,
+      description: ''
+    }
+  ];
 
+  slideContainer = [this.slides1, this.slides2];
+
+  currentSlide = 0;
+  constructor() {   this.slides= this.slides1; }
+  
+  ngOnInit(): void { }
+    previousSlide(slide: string) {
+      if(slide === 'slide1'){
+        this.slides= this.slides1;
+      }
+
+      if(slide === 'slide2'){
+        this.slides= this.slides1;
+      }
+      if(this.currentSlide > 0){
+        this.currentSlide =   this.currentSlide - 1 ;
+      } else {
+        this.currentSlide  = this.slides.length - 1;
+      }
+
+    }
+  
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.slides1.length;
+    }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { finalize, from, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -7,11 +9,25 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private elRef: ElementRef) { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
+
   scrollToSection(sectionId: string): void {
+    this.redirectSection(sectionId)
+      .pipe(
+        finalize(() => this.scrollSection(sectionId))
+      )
+      .subscribe();
+  }
+
+  redirectSection(sectionId: string): any {
+    if (sectionId === 'contacto' || sectionId === 'mapa') {
+      return from(this.router.navigate(['/'])); 
+    }
+  }
+
+  scrollSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
